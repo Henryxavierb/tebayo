bd4eba3f53025bf90aa118448f5635fe48be4661
Henrique Barros <henriquexavierb@gmail.com>
2021-01-25 11:47:29 -0300

    [hb]chore: add logo

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Input} from 'antd';
+import Link from 'next/link';
 require('../styles/search.bar.less');
 
 function SearchBar(props) {
@@ -7,22 +8,28 @@ function SearchBar(props) {
 	
 	return (
 		<form className="navbar" onSubmit={(event) => event.preventDefault()}>
-			<Input
-				size="large"
-				value={search}
-				className="search-bar-input"
-				placeholder="Search anime (Type only anime's name)"
-				onChange={(event) => updateSearch(event.target.value)}
-			/>
+			<Link href='/'>
+				<a>Tebayo</a>
+			</Link>
 			
-			<button
-				size="large"
-				type="primary"
-				className="search-bar-button"
-				onClick={() => filterAnime()}
-			>
-				Buscar
-			</button>
+			<div className="search">
+				<Input
+					size="large"
+					value={search}
+					className="search-bar-input"
+					placeholder="Search anime (Type only anime's name)"
+					onChange={(event) => updateSearch(event.target.value)}
+				/>
+				
+				<button
+					size="large"
+					type="primary"
+					className="search-bar-button"
+					onClick={() => filterAnime()}
+				>
+					Buscar
+				</button>
+			</div>
 		</form>
 	)
 }
